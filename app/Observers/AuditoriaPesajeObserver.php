629f156f91d481dc527da6c3371fa4462e9dd5ce
ASHLEY DARIANA NARVAEZ UMANA <ASHLEY.NARVAEZ@ucr.ac.cr>
2026-05-18 15:19:07 -0600

    xdd

--- app/Observers/AuditoriaPesajeObserver.php
+++ app/Observers/AuditoriaPesajeObserver.php
@@ -9,7 +9,7 @@ class AuditoriaPesajeObserver
 {
     public function created(Pesaje $pesaje): void
     {
-        // Registra de forma agnóstica el rastro de auditoría en los archivos de Log del sistema
+        // Registra el rastro de auditoría en los archivos de Log del sistema
         Log::info("AUDITORÍA SISTEMA: El usuario del sistema registró el pesaje 
         ID {$pesaje->id_pesaje} 
         con un valor de {$pesaje->peso}kg.");
